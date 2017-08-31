3fa633992e5dab773e2739d01bdaceef8bf19809
xuliang <1209066811@qq.com>
2017-08-31 14:08:38 +0800

    添加注释

---
+++ Custom.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Reflection;    //引用BindingFlags需要
+
+namespace TestAttribute
+{
+    /// <summary>
+    /// 使用自定义属性，并获取方法的属性
+    /// </summary>
+    public class Custom
+    {
+        public Custom()
+        {
+            //这是一个反射
+            var syncAttribute = Attribute.GetCustomAttribute(this.GetType().GetMethod(nameof(Working), BindingFlags.NonPublic | BindingFlags.Instance), typeof(SyncWorkAttribute)) as SyncWorkAttribute;
+            if (syncAttribute != null)
+            {
+                _enableSyncWork = true;
+            }
+            else
+            {
+                var asyncAttribute = Attribute.GetCustomAttribute(this.GetType().GetMethod(nameof(Working), BindingFlags.NonPublic | BindingFlags.Instance), typeof(AsyncWorkAttribute)) as AsyncWorkAttribute;
+                if (asyncAttribute != null)
+                {
+                    _enableSyncWork = false;
+                }
+            }
+        }
+
+        public bool _enableSyncWork { get; }
+
+        [SyncWork]
+        protected void Working()
+        {
+
+        }
+
+
+        public static bool TEST_CUSTOM()
+        {
+            Custom custom = new Custom();
+            return custom._enableSyncWork;
+        }
+    }
+
+    /// 前面使用了SyncWorkAttribute和AsyncWorkAttribute两个属性，需要在这定义
+
+    /// <summary>
+    /// 使用同步的方式执行
+    /// </summary>
+    /// <seealso cref="System.Attribute" />
+    [AttributeUsage(AttributeTargets.Method)]
+    public class SyncWorkAttribute : Attribute
+    {
+    }
+
+    /// <summary>
+    /// 使用异步的方式执行
+    /// </summary>
+    /// <seealso cref="System.Attribute" />
+    [AttributeUsage(AttributeTargets.Method)]
+    public class AsyncWorkAttribute : Attribute
+    {
+    }
+
+}
---
+++ Program.cs
@@ -0,0 +1,55 @@
+﻿//#define DEBUG
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.Runtime.InteropServices;
+using System.Diagnostics;
+
+
+namespace TestAttribute
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //方式1
+            bool isSync = Custom.TEST_CUSTOM();
+            Console.WriteLine(isSync);
+
+            ////方式2
+            DisplayRunningMessage();
+            DisplayDebugMessage();
+
+            //
+            MessageBox(0, "Hello", "Message", 0);
+
+
+            Console.ReadLine();
+        }
+
+
+        //DllImport Attribute表明了MessageBox是User32.DLL中的函数，
+        [DllImport("User32.dll")]
+        public static extern int MessageBox(int hParent, string Message, string Caption, int Type);
+
+        //Conditional对满足参数的定义条件的代码进行编译，如果没有定义DEBUG,那么该方法将不被编译
+        [Conditional("DEBUG")]
+        private static void DisplayRunningMessage()
+        {
+            Console.WriteLine("开始运行Main子程序。当前时间是" + DateTime.Now);
+        }
+
+        //Obsolete表明了DispalyDebugMessage方法已经过时了，它有一个更好的方法来代替它，
+        //当我们的程序调用一个声明了Obsolete的方法时，那么编译器会给出信息，Obsolete还有其他两个重载的版本
+        [Conditional("DEBUG")]
+        [Obsolete]
+        private static void DisplayDebugMessage()
+        {
+            Console.WriteLine("开始Main子程序");
+        }
+
+    }
+}
